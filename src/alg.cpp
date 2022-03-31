9a2cdf9e24645bb645d08e44429ca5f90fb56072
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2022-03-31 15:16:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,58 @@
 #include <map>
 #include "tstack.h"
 
+int prior(char op) {
+    switch (op) {
+      case '(': return 0; 
+      case ')': return 1; 
+      case '+': case '-': return 2; 
+      case '*': case '/': return 3; 
+      default: return 4;
+    }
+}
+  
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  TStack <char, 100> stack;
+    std::string result = "";
+    
+    for (int i = 0; i < inf.length(); i++) {
+        char sym = inf[i];
+        int symPrior = prior(sym);
+        if (symPrior == 4) {
+            result += sym;
+            result += ' ';
+        } else {
+            if (symPrior == 0 || symPrior > prior(stack.get()) || stack.isEmpty()) {
+                stack.push(sym);
+            } else if (symPrior == 1) {
+                while (prior(stack.get()) != 0) {
+                    result += stack.get();
+                    result += ' ';
+                    stack.pop();
+                }
+                 stack.pop();
+            } else if (symPrior <= prior(stack.get())) {
+                if (symPrior <= prior(stack.get())) {
+                    result += stack.get();
+                    result += ' ';
+                    stack.pop();
+                }
+                stack.push(sym);
+            }
+        }
+    }
+    if (!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
+            result += stack.get();
+            result += ' ';
+            stack.pop();
+        }
+        result.pop_back();
+    }
+    return result;
 }
 
 int eval(std::string pref) {
-  // добавьте код
+  
   return 0;
 }
